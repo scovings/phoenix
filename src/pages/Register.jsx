2129de3abf4e5cf1169249b3793e109f7edfa873
sco. <77859647+scovings@users.noreply.github.com>
2025-03-28 17:27:56 +0000

    base

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2025 scovings
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
